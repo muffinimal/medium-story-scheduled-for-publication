20791953b184e7b2a5977191dfaa5080d6848ca6
Martin van Soest <muffinimal@gmail.com>
2021-09-28 16:41:46 +0200

    Display scheduled publication date

--- foreground.js
+++ foreground.js
@@ -1,8 +1,4 @@
-// This script gets injected into any opened page
-// whose URL matches the pattern defined in the manifest
-// (see "content_script" key).
-// Several foreground scripts can be declared
-// and injected into the same or different pages.
+const scriptdata = document.evaluate("/html/body/script[contains(., 'scheduledPublishAt')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
 
 console.log("This prints to the console of the page (injected only if the page url matched)")
 
@@ -14,27 +10,71 @@ window.addEventListener('load', (event) => {
 });
 
 function checkSchedule() {
-    var scriptdata = document.evaluate("/html/body/script[contains(., 'scheduledPublishAt')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
     // /html/body/script[contains(.,"scheduled")] //scheduledPublishedAt
+    data = getdata();
 
-    if (scriptdata.singleNodeValue == null) {
+    if (data == null) {
         console.log("Story not scheduled for publication.");
     } else {
         console.log("Story scheduled for publication");
 
-        let cdata = extractCData(scriptdata);
-
-        jsonobj = JSON.parse(cdata);
-
-        let publishDateUnix = jsonobj["scheduledPublishAt"];
+        let publishDateUnix = data["scheduledPublishAt"];
         let publishDate = new Date(publishDateUnix).toLocaleDateString();
+        let publishTime = new Date(publishDateUnix).toLocaleTimeString();
 
         console.log(publishDate);
+
+        createHTML(publishDate, publishTime);
     }
 }
+
+function getdata(){
+    if( scriptdata.singleNodeValue == null ) {
+        return false;
+    } else {
+        let cdata = extractCData(scriptdata);
+        return JSON.parse(cdata);
+    }
+
+}
+
+function getPublication() {
+    //"homeCollectionId": "2cef85d78a7f", --> 10 in 10. Should test while in Dev|Fiant
+
+}
+
+/**
+ * 
+ * Butter-bar message 
+ * <div class="butterBar" data-action-scope="_actionscope_1">
+ *      <div class="butterBar-message">
+ *          Your story is scheduled to publish on Thursday, Oct 7 at 4:50pm.
+ *      </div>
+ * </div>} 
+ * @returns 
+ */
+
 function extractCData(data) {
     let cdata = data.singleNodeValue.innerText.slice(31);
     cdata = cdata.substr(0, cdata.length - 8);
 
     return cdata;
+}
+
+function createHTML(date, time) {
+    let mainArticle = document.querySelectorAll('main[role="main"] > article');
+
+    let scheduledMessageDiv = document.createElement('div');
+    scheduledMessageDiv.className = "scheduledMessageDiv";
+
+    let scheduledMessage = document.createElement('span');
+    scheduledMessage.className = "scheduledMessage";
+
+    scheduledMessage.innerText = `Scheduled to be published at ${date} at ${time}`;
+
+    scheduledMessageDiv.appendChild(scheduledMessage);
+
+//<div class="butterBar-message">Your story is scheduled to publish on Thursday, Oct 7 at 4:54pm.</div>
+
+    mainArticle[0].prepend(scheduledMessageDiv);
 }
