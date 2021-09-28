2a0400017070e731383080d978c6718d16a965f4
muffinimal <35263627+muffinimal@users.noreply.github.com>
2021-09-29 00:55:44 +0200

    Refactor and getting publication

--- foreground.js
+++ foreground.js
@@ -14,44 +14,8 @@ function extractCData(data) {
     return cdata;
 }
 
-// let first wait for the page to fully load
-/**
- * TODO: wachttijd is niet nodig, gewoon wachten op ful page load
- */
-window.addEventListener('load', (event) => {
-    console.log("waiting");
-    //setTimeout(checkSchedule(), 500);
-    checkSchedule();
-});
-
-function checkSchedule() {
-    // /html/body/script[contains(.,"scheduled")] //scheduledPublishedAt
-    data = getdata();
-
-    if (data == null) {
-        console.log("Story not scheduled for publication.");
-    } else {
-        console.log("Story scheduled for publication");
-
-        publishDate = getDateTimeSchedule();
-
-        console.log(publishDate);
-
-        createHTML(publishDate);
-    }
-}
-
-function getDateTimeSchedule( datetime ) {
-    let publishDateUnix = data["scheduledPublishAt"];
-    let publishDate = new Date(publishDateUnix).toLocaleDateString();
-    let publishTime = new Date(publishDateUnix).toLocaleTimeString();
-
-    return [publishDate, publishTime];
-}
-
-
-function getdata(){
-    if( scriptdata.singleNodeValue == null ) {
+function getData() {
+    if (scriptdata.singleNodeValue == null) {
         return false;
     } else {
         let cdata = extractCData(scriptdata);
@@ -59,27 +23,63 @@ function getdata(){
     }
 }
 
+// let first wait for the page to fully load
+/**
+ * TODO: wachttijd is niet nodig, gewoon wachten op ful page load
+ */
+window.addEventListener('load', (event) => {
+    console.log("waiting for page to fully load");
+    //setTimeout(checkSchedule(), 500);
+    checkSchedule();
+});
+
+function checkSchedule() {
+    if (data = getData()) {
+        console.log("Story scheduled for publication");
+
+        publishDate = getDateTimeSchedule();
+        createHTML(publishDate);
+
+        getPublication();
+    } else {
+        console.log("Story not scheduled for publication.");
+    }
+}
+
+function getDateTimeSchedule(datetime) {
+    let publishDateUnix = data["scheduledPublishAt"];
+    let publishDate = new Date(publishDateUnix).toLocaleDateString();
+    let publishTime = new Date(publishDateUnix).toLocaleTimeString();
+
+    return [publishDate, publishTime];
+}
+
 function getPublication() {
     //"homeCollectionId": "2cef85d78a7f", --> 10 in 10. Should test while in Dev|Fiant
-/*     "homeCollection": {
-        "id": "2cef85d78a7f",
-        "name": "10 in 10 Challenge",
-        "slug": "10-in-10-challenge",
-        "tags": [
-          "WRITING",
-          "CHALLENGE",
-          "LEARN",
-          "STORIES",
-          "FUN"
-        ], */
-    publicationId = data[homeCollectionId];
-    console.log( publicationId );
+    /*     "homeCollection": {
+            "id": "2cef85d78a7f",
+            "name": "10 in 10 Challenge",
+            "slug": "10-in-10-challenge",
+            "tags": [
+              "WRITING",
+              "CHALLENGE",
+              "LEARN",
+              "STORIES",
+              "FUN"
+            ], */
+    // let publicationId = data[homeCollectionId];
+    //console.log(publicationId);
 
+    let collectionObj = data.references.Collection;
+    let collection = Object.entries(collectionObj).map(([key, value]) => ({ key, ...value }));
+
+    console.info(collection);
+    console.log(collection[0].name);
 }
 
 function getReviewStatus() {
-/*     "virtuals": {
-        "statusForCollection": "PENDING", */
+    /*     "virtuals": {
+            "statusForCollection": "PENDING", */
 }
 
 
@@ -97,7 +97,7 @@ function createHTML(datetime) {
 
     scheduledMessageDiv.appendChild(scheduledMessage);
 
-//<div class="butterBar-message">Your story is scheduled to publish on Thursday, Oct 7 at 4:54pm.</div>
+    //<div class="butterBar-message">Your story is scheduled to publish on Thursday, Oct 7 at 4:54pm.</div>
 
     mainArticle[0].prepend(scheduledMessageDiv);
 }
