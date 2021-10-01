039b0cbbee98f933ba4ceb9b338a5c829db69c3d
muffinimal <35263627+muffinimal@users.noreply.github.com>
2021-10-01 09:28:48 +0200

    Updated onupdate url, version

--- service-worker.js
+++ service-worker.js
@@ -4,13 +4,13 @@ chrome.runtime.onInstalled.addListener((reason) => {
 	switch(reason.reason) {
 		case chrome.runtime.OnInstalledReason.UPDATE:
 			chrome.tabs.create({
-				url: 'https://medium.com/@muffinimal/distribution-extension-update-2-1-4b02bac767a6'
+				url: 'https://medium.com/@muffinimal/scheduled-for-publication-chrome-extension-9a6b5d263b98'
 			  });
 			  console.log( "[MediumDistribution] Updating Medium Distribution info to: ", chrome.runtime.getManifest().version, "from ", reason.previousVersion );
 			break;
 		case chrome.runtime.OnInstalledReason.INSTALL:
 				chrome.tabs.create({
-					url: 'https://medium.com/@muffinimal/distribution-extension-update-2-1-4b02bac767a6'
+					url: 'https://medium.com/@muffinimal/scheduled-for-publication-chrome-extension-9a6b5d263b98'
 				  });
 				  console.log( "[MediumDistribution] New install: ", chrome.runtime.getManifest().version );
 				break;
